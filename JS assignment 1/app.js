fa7d50142070e75a0bca8493d6828060d8625118
Wajahat Hussain <51961119+wajahat-abid-hussain@users.noreply.github.com>
2023-01-26 19:42:09 +0500

    Add files via upload

---
+++ JS assignment 1/app.js
@@ -0,0 +1,31 @@
+//Chapter: 1 Alert
+
+
+
+
+//Question 1
+
+alert ("Hello  & welcome")
+
+
+//Question: 2
+
+alert ("Error! Please enter a valid password.")
+
+
+//Question:3
+
+
+alert ("Welcome to JS Land... \n Happy Coding!")
+
+
+//Question:4
+
+
+alert (" Welcome to JS Land...")
+
+
+//Question:5
+
+
+alert ("Hello.. I can run JS through my web brower's console")
